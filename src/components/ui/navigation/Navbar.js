cbd2817ce7000a5bd9ef67ee913c2d34e2dded47
Omar De Angelis <omar.hpvfilm@gmail.com>
2022-02-19 00:01:21 +0100

    fix: desktop navbar style

--- src/components/ui/navigation/Navbar.js
+++ src/components/ui/navigation/Navbar.js
@@ -49,54 +49,10 @@ const StyledNav = styled.nav`
   }
 `;
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    display: "none",
-    [theme.breakpoints.down("sm")]: {
-      display: "block",
-      backgroundColor: theme.palette.secondary.main,
-      marginRight: theme.spacing(2),
-    },
-  },
-  title: {
-    flexGrow: 1,
-    width: "fit-content",
-    fontWeight: "600",
-    color: theme.palette.primary.main,
-  },
-  navItems: {
-    paddingTop: "1rem",
-    flexGrow: 1,
-    height: "fit-content",
-  },
-  navLink: {
-    fontWeight: 600,
-    textAlign: "center",
-  },
-  toggleBtn: {
-    display: "flex",
-    justifyContent: "flex-end",
-    [theme.breakpoints.up("md")]: {
-      display: "none",
-    },
-  },
-  linksContainer: {
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
-    },
-  },
-}));
 const Navbar = () => {
-  const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(null);
   const data = useStaticQuery(query);
   const { mediaQuery, toggleSidebar } = useGlobalContext();
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
 
   const handleClose = React.useCallback(() => {
     setAnchorEl(null);
@@ -183,124 +139,9 @@ const Navbar = () => {
             </Stack>
           </Stack>
         </Container>
-        <Menu
-          id='simple-menu'
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuList>
-            {data.allContentfulCorsi.nodes.map((node) => {
-              return (
-                <MenuItem
-                  key={node.slug}
-                  onClick={handleClose}
-                  component={GastbyLink}
-                  to={`/${node.slug}/`}
-                >
-                  <ListItemIcon>
-                    {node.categoria === "videomaking" ? (
-                      <VideoCallIcon fontSize='small' color='primary' />
-                    ) : (
-                      <CodeIcon fontSize='small' color='primary' />
-                    )}{" "}
-                  </ListItemIcon>
-                  <Typography variant='button'>{node.categoria}</Typography>
-                </MenuItem>
-              );
-            })}
-          </MenuList>
-        </Menu>
       </StyledNav>
       <Box height='72px' width='100px'></Box>
     </>
-    /* <AppBar
-        position='static'
-        sx={{
-          background: "white",
-        }}
-        elevation={0}
-      >
-        <Container component={Toolbar} maxWidth='lg'>
-          <Grid
-            container
-            justify='space-between'
-            alignItems='center'
-            className={classes.navItems}
-          >
-            <Grid item md={mediaQuery.sm ? 6 : 2}>
-              <GastbyLink to='/'>
-                <StaticImage
-                  src='../../../images/logo.png'
-                  alt='Logo Hpv 4 Learning'
-                  placeholder='tracedSVG'
-                  layout='fixed'
-                  height={70}
-                  width={70}
-                />
-              </GastbyLink>
-            </Grid>
-            <Grid item xs={6} className={classes.toggleBtn}>
-              <IconButton onClick={toggleSidebar}>
-                <MenuIcon />
-              </IconButton>
-            </Grid>
-            <Grid
-              item
-              container
-              md={10}
-              spacing={4}
-              className={classes.linksContainer}
-            >
-              <Grid item sm={4}>
-                <Button
-                  className={classes.navLink}
-                  color='inherit'
-                  size='large'
-                  variant='text'
-                  startIcon={<HomeIcon color='primary' />}
-                  component={GastbyLink}
-                  to='/'
-                >
-                  {" "}
-                  Home{" "}
-                </Button>
-              </Grid>
-              <Grid item sm={4}>
-                <Button
-                  className={classes.navLink}
-                  color='inherit'
-                  variant='text'
-                  startIcon={<SchoolIcon color='primary' />}
-                  size='large'
-                  aria-controls='fade-menu'
-                  aria-haspopup='true'
-                  onClick={handleClick}
-                  endIcon={<ArrowDropDownIcon />}
-                >
-                  Corsi
-                </Button>
-              
-              </Grid>
-              <Grid item sm={4}>
-                <Button
-                  className={classes.navLink}
-                  color='inherit'
-                  size='large'
-                  startIcon={<AssignmentIndIcon color='primary' />}
-                  variant='text'
-                  component={GastbyLink}
-                  to='/about/'
-                >
-                  {" "}
-                  Chi Siamo
-                </Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Container>
-      </AppBar>*/
   );
 };
 
