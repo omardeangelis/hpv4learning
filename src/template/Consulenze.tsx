ccabe1989252d81089568e3f3b66f3233e8764e5
leonardodemarco <demarco.leonardo2000@gmail.com>
2022-08-31 16:04:05 +0200

    feat: create server-side route 'consulenze'

---
+++ src/template/Consulenze.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/shared/layout";
+import { Button } from "@mui/material";
+
+const Consulenze = () => {
+  return (
+    <>
+      <Layout>
+        <Button>ciao</Button>
+      </Layout>
+    </>
+  );
+};
+
+export default Consulenze;
