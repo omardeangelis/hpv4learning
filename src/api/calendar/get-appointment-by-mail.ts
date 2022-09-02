750279b5383a93824420f092971e71387d0fde1e
Omar De Angelis <omardeangelis@odeangelis.local>
2022-09-02 10:47:04 +0200

    fix: get appointmento-by-mail now use GET method

--- src/api/calendar/get-appointment-by-mail.ts
+++ src/api/calendar/get-appointment-by-mail.ts
@@ -6,22 +6,22 @@ import { HttpMethod } from "../../server/types";
 
 type ReqProps = {
   method: HttpMethod;
-  body: {
-    mail: string;
+  query: {
+    email: string;
   };
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const handler = (req: ReqProps, res: any) => {
-  if (req.method !== "POST" && process.env.NODE_ENV === "production")
-    throw new Error("Use POST Method");
-
-  if (!isValidMail(req?.body?.mail)) {
+  if (req.method !== "GET" && process.env.NODE_ENV === "production")
+    throw new Error("Use GET Method");
+  const userMail =
+    process.env.NODE_ENV === "production" && req.query.email
+      ? req.query.email
+      : testingMail;
+  if (!isValidMail(userMail)) {
     if (process.env.NODE_ENV === "production") throw new Error("Invalid Mail");
   }
-
-  const userMail =
-    process.env.NODE_ENV === "production" ? req.body.mail : testingMail;
 
   userHasApointment(userMail)
     .then((response) => res.status(200).json(response))
