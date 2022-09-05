f15232a46958de1cc17e1c9d7e67988c969181d2
Omar De Angelis <omardeangelis@o.wind3.hub>
2022-09-05 11:01:07 +0200

    feat: pure css basic animation btn float

--- src/feature/consulenze/components/Hero.tsx
+++ src/feature/consulenze/components/Hero.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import { Box, Container } from "@mui/system";
 import styled from "@emotion/styled";
 import { Typography } from "@mui/material";
 import { RoundedButton } from "../../../components/layout";
 import { useConsulenzeContext } from "../context";
+import useHasMounted from "../../../hook/useHasMounted";
+import VideoCallIcon from "@mui/icons-material/VideoCall";
 
 const StyledBox = styled(Box)({
   backgroundImage:
@@ -14,10 +16,85 @@ const StyledBox = styled(Box)({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
+  ".float-icon": {
+    display: "none",
+  },
+  "#placeholder-cta-btn": {
+    display: "none",
+  },
+  ".start-float": {
+    position: "fixed",
+    zIndex: 3,
+    bottom: "20px",
+    right: "20px",
+    ".float-icon": {
+      display: "block !important",
+    },
+    ".float-text": {
+      display: "none",
+    },
+  },
 });
 
 export const Hero = () => {
   const { openModal } = useConsulenzeContext() || {};
+  const ref = React.useRef<HTMLDivElement>();
+  const hasMounted = useHasMounted();
+  const buttonElement = React.useMemo(() => {
+    if (hasMounted) {
+      return document.querySelector("#main-cta-btn");
+    }
+  }, [hasMounted]);
+  const placeholderBtn = React.useMemo(() => {
+    if (hasMounted) {
+      return document.querySelector(
+        "#placeholder-cta-btn",
+      ) as HTMLButtonElement;
+    }
+  }, [hasMounted]);
+  const [animationCompleted, setAnimationCompleted] = React.useState(false);
+
+  const handleButtonAnimation = React.useCallback(() => {
+    if (!hasMounted) return;
+    const windowY = window.innerHeight;
+    const { top, bottom } = ref.current?.getBoundingClientRect() || {};
+    if (!buttonElement || !placeholderBtn) return;
+
+    if (!animationCompleted) {
+      if (top && top <= windowY - window.screen.height) {
+        buttonElement.classList.remove("unfloat-animation");
+        buttonElement.classList.add("start-float");
+        buttonElement.classList.add("float-animation");
+        placeholderBtn.style.display = "block";
+        setTimeout(() => setAnimationCompleted(true), 750);
+      }
+    } else {
+      if (bottom && top && bottom > windowY - window.screen.height) {
+        placeholderBtn.style.display = "none";
+
+        buttonElement.classList.remove("start-float");
+        buttonElement.classList.remove("float-animation");
+        buttonElement.classList.add("unfloat-animation");
+
+        setTimeout(() => setAnimationCompleted(false), 750);
+      }
+    }
+  }, [
+    hasMounted,
+    buttonElement,
+    placeholderBtn,
+    buttonElement?.classList,
+    animationCompleted,
+    ref.current?.getBoundingClientRect(),
+    ref,
+  ]);
+
+  useEffect(() => {
+    if (hasMounted) {
+      window.addEventListener("scroll", handleButtonAnimation);
+      return () => window.removeEventListener("scroll", handleButtonAnimation);
+    }
+  }, [hasMounted, handleButtonAnimation]);
 
   return (
     <StyledBox>
@@ -123,6 +200,7 @@ export const Hero = () => {
               </Typography>
             </Box>
             <Box
+              ref={ref}
               sx={{
                 mt: { xs: "24px", lg: "34px" },
               }}
@@ -130,6 +208,7 @@ export const Hero = () => {
               justifyContent='center'
             >
               <RoundedButton
+                id='placeholder-cta-btn'
                 variant='contained'
                 size='large'
                 color='secondary'
@@ -137,13 +216,40 @@ export const Hero = () => {
                 sx={{
                   background: "#fff",
                   color: "purple.400",
+                  transition: "background 200ms ease",
+                  height: {
+                    xs: "56px",
+                    lg: "64px",
+                    opacity: "0",
+                  },
+                }}
+              >
+                Fissa una chiamata
+              </RoundedButton>
+              <RoundedButton
+                id='main-cta-btn'
+                variant='contained'
+                size='large'
+                color='secondary'
+                onClick={openModal}
+                sx={{
+                  background: "#fff",
+                  color: "purple.400",
+                  transition: "background 200ms ease",
                   height: {
                     xs: "56px",
                     lg: "64px",
                   },
                 }}
               >
-                Fissa una chiamata
+                <span className='float-text'>Fissa una chiamata</span>
+                <VideoCallIcon
+                  className='float-icon'
+                  sx={{
+                    fontSize: { xs: "24px", lg: "36px" },
+                    fill: "#fff",
+                  }}
+                />
               </RoundedButton>
             </Box>
           </Box>
