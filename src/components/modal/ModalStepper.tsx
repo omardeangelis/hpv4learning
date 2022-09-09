a8019c90cdb771dcf7689ab956c60bd579fe060f
leonardodemarco <demarco.leonardo2000@gmail.com>
2022-09-09 17:52:43 +0200

    use stepIndex from useModalContext as activeStep in Stepper component

--- src/components/modal/ModalStepper.tsx
+++ src/components/modal/ModalStepper.tsx
@@ -3,16 +3,17 @@ import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
+import { useModalContext } from "./context";
 
 type Props = {
-  step: number;
   labels: string[];
 };
 
-export const ModalStepper = ({ step, labels }: Props) => {
+export const ModalStepper = ({ labels }: Props) => {
+  const stepIndex = useModalContext();
   return (
     <Box sx={{ width: "100%" }}>
-      <Stepper activeStep={step}>
+      <Stepper activeStep={stepIndex as unknown as number}>
         {labels.map((label, index) => {
           return (
             <Step key={index}>
