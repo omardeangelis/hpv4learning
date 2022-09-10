95480a69ad17def5bad319945ed74a9866dcd31e
Omar De Angelis <omardeangelis@o.wind3.hub>
2022-09-11 01:28:30 +0200

    feat: standard query

--- src/template/Consulenze.tsx
+++ src/template/Consulenze.tsx
@@ -3,6 +3,9 @@ import Layout from "../components/shared/layout";
 import { Router } from "@reach/router";
 import { ConsulenzeLayout } from "../feature/pages/ConsulenzeLayout";
 import { ReservationModal } from "../feature/consulenze/components";
+import MetaDecorator from "../components/SEO/components/MetaDecorator";
+import WebPageSchema from "../components/SEO/components/WebPageSchema";
+import LinkHandler from "../components/SEO/components/LinkHandler";
 
 const Consulenze = () => {
   return (
@@ -13,6 +16,36 @@ const Consulenze = () => {
         </Router>
         <ConsulenzeLayout />
       </Layout>
+    </>
+  );
+};
+
+export const Head = () => {
+  const breadcrumbs = React.useMemo(() => {
+    return [
+      {
+        text: "Home",
+        link: "/",
+      },
+      {
+        text: "Contattaci",
+        link: "consulenze",
+      },
+    ];
+  }, []);
+  return (
+    <>
+      <MetaDecorator
+        metaTitle='Contattaci: Siti Web professinali'
+        metaDescription='Fissa una chiamata per studiare con noi la miglior soluzione e sviluppare un preventivo misurato sulle tue esigenze'
+        disableSlogan
+      />
+      <WebPageSchema
+        title='Contattaci: Siti Web professinali'
+        description='Fissa una chiamata per studiare con noi la miglior soluzione e sviluppare un preventivo misurato sulle tue esigenze'
+        breadcrumbs={breadcrumbs}
+      />
+      <LinkHandler />
     </>
   );
 };
