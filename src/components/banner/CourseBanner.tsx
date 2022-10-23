4a621034076b53464e1a73fcb5a9626feb5582e4
Leonardo De Marco <leonardo.demarco@emea.nttdata.com>
2022-10-23 14:20:56 +0200

    fix: material ui imports in banner folder

--- src/components/banner/CourseBanner.tsx
+++ src/components/banner/CourseBanner.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 import {
   GatsbyImage,
   IGatsbyImageData,
