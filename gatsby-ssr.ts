aeae9c9b3ebb275157adaf691e2cac206a159ebe
omardeangelis <omar.hpvfilm@gmail.com>
2022-11-28 23:45:49 +0100

    feat: try to solve iubenda method error

--- gatsby-ssr.ts
+++ gatsby-ssr.ts
@@ -1,14 +1,14 @@
-import "./src/styles/global.css";
-import "./src/styles/font.css";
-import { GlobalProvider } from "./GlobalProvider";
-import { GatsbySSR } from "gatsby";
-import { useServeStaticFonts } from "./useServeStaticFonts";
-import { injectIubenda } from "./iubenda";
+import "./src/styles/global.css"
+import "./src/styles/font.css"
+import { GlobalProvider } from "./GlobalProvider"
+import { GatsbySSR } from "gatsby"
+import { useServeStaticFonts } from "./useServeStaticFonts"
+import { injectIubenda } from "./iubenda"
 export const onRenderBody: GatsbySSR["onRenderBody"] = ({
   setHeadComponents,
   setHtmlAttributes,
 }) => {
-  setHeadComponents([...useServeStaticFonts(), ...injectIubenda()]);
-  setHtmlAttributes({ lang: "it" });
-};
-export const wrapRootElement: GatsbySSR["wrapRootElement"] = GlobalProvider;
+  setHeadComponents([...useServeStaticFonts(), ...injectIubenda()])
+  setHtmlAttributes({ lang: "it" })
+}
+export const wrapRootElement: GatsbySSR["wrapRootElement"] = GlobalProvider
--- src/components/shared/layout.tsx
+++ src/components/shared/layout.tsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React from "react"
 //Custom Component
-import { Navbar, Sidebar } from "../../feature/navigation/components";
-import { Footer } from "../../feature/footer";
+import { Navbar, Sidebar } from "../../feature/navigation/components"
+import { Footer } from "../../feature/footer"
+import { Script } from "gatsby"
 const Layout = ({
   children,
   disableColor,
   enableFooterPadding,
 }: {
-  children: React.ReactNode;
-  disableColor?: true;
-  enableFooterPadding?: true;
+  children: React.ReactNode
+  disableColor?: true
+  enableFooterPadding?: true
 }) => {
   return (
     <>
       <Navbar disableColor={disableColor} />
       <Sidebar />
       <main>{children}</main>
-
       <Footer enableFooterPadding={enableFooterPadding} />
-    </>
-  );
-};
+      <Script
+        id="iubenda-1"
+        key="iubenda-1"
+        type="text/javascript"
+        src="cdn.iubenda.com/cs/ccpa/stub.js"
+      />
 
-export default Layout;
+      <Script
+        id="iubenda-2"
+        key="iubenda-2"
+        type="text/javascript"
+        src="cdn.iubenda.com/cs/iubenda_cs.js"
+        async
+        onLoad={() => {
+          globalThis._iub.cc = "EU"
+        }}
+      />
+    </>
+  )
+}
+
+export default Layout
